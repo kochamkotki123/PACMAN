71f320a846a6644d69abbfed6c8e0d878b166637
kochamkotki123 <105227633+kochamkotki123@users.noreply.github.com>
2022-05-21 18:41:48 +0200

    Add files via upload

---
+++ wip.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1000,800))
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+
+
+    pygame.display.update()
