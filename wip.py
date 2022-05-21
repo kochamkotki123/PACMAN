3463469037f7d74981b51fa5b6a82b163349a9a1
kochamkotki123 <105227633+kochamkotki123@users.noreply.github.com>
2022-05-21 18:41:13 +0200

    Delete wip.py

--- wip.py
+++
@@ -1,8 +0,0 @@
-print("podaj ciąg znaków")
-CIAG = input()
-SAMOG = 0
-for litera in CIAG:
-    if litera in ('e','y','u','i','o','a'):
-        SAMOG += 1
-print("w podanym ciągu jest",SAMOG,"samogłosek")
-#bez polskich znaków
