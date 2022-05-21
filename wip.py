9c26a1d0ac0033d62655a529e7681f2434f761b5
kochamkotki123 <105227633+kochamkotki123@users.noreply.github.com>
2022-05-21 20:01:34 +0200

    Update wip.py
    ogarniam sb podstawy pygame, dokończę jak zjem obiad

--- wip.py
+++ wip.py
@@ -1,14 +1,16 @@
 import pygame
+from sys import exit
 
 pygame.init()
-
-screen = pygame.display.set_mode((1000,800))
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("PACMAN")
+clock = pygame.time.Clock()
 
 
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
-
-
+            exit()
     pygame.display.update()
+    clock.tick(60)
