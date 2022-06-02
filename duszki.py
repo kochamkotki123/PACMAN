b89295196e578a4c169172360427e43517ab049b
gaba23333 <104887032+gaba23333@users.noreply.github.com>
2022-06-02 17:18:19 +0200

    Duszki
    Plik z kodem dotyczącym wrogów

---
+++ duszki.py
@@ -0,0 +1,35 @@
+import pygame
+from ustawienia import *
+vec = pygame.math.Vector2
+
+# cała klasa duszków, ich ilość, pozycje
+class Ghost:
+    def __init__(self, app, pos, number):
+        self.app = app
+        self.grid_pos = pos
+        self.pix_pos = self.get_pix_pos()
+        self.radius = int(self.app.cell_width//2.3)
+        self.number = number
+        self.colour = self.set_colour()
+
+
+
+    def update(self):
+        pass
+# umieszczenie duszków na ekranie
+    def draw(self):
+        pygame.draw.circle(self.app.screen, self.colour,(int(self.pix_pos.x), int(self.pix_pos.y)) ,self.radius)
+# znowu pozycjonowanie duszków
+    def get_pix_pos(self):
+        return vec((self.grid_pos.x*self.app.cell_width)+TOP_BOTTOM_BUFFER//2+self.app.cell_width//2, (self.grid_pos.y*self.app.cell_height)+ TOP_BOTTOM_BUFFER//2+self.app.cell_height//2)
+        print(self.grid_pos, self.pix_pos)
+# każda cyfra to osobny duszek i jego kolor, jeszcze nie ogarnęłam jak wrzucić w to avatary
+    def set_colour(self):
+        if self.number == 0:
+            return (43, 78, 203)
+        if self.number == 1:
+            return (255, 192, 203)
+        if self.number == 2:
+            return (189, 29, 29)
+        if self.number == 3:
+            return (255, 69, 0)
