d6e752fe1de0679ad33d27c418edc97f4f6155f7
gaba23333 <104887032+gaba23333@users.noreply.github.com>
2022-06-02 17:19:03 +0200

    Add files via upload

---
+++ ustawienia.py
@@ -0,0 +1,23 @@
+
+from pygame.math import Vector2 as vec
+# ustawienia ekranu
+WIDTH, HEIGHT = 610, 670
+FPS = 60
+TOP_BOTTOM_BUFFER = 50
+MAZE_WIDTH, MAZE_HEIGHT = WIDTH-TOP_BOTTOM_BUFFER, HEIGHT - TOP_BOTTOM_BUFFER
+
+# ustawienia kolorów
+BLACK = (0, 0, 0)
+RED = (208, 22, 22)
+GREY = (107, 107, 107)
+WHITE = (255,255,255)
+PLAYER_COLOUR = (190,194,15)
+# ustawienia czcionki
+START_TEXT_SIZE = 16
+START_FONT = 'arial black'
+
+
+# ustawienia związane z graczem
+
+PLAYER_START_POS = 0
+# ustawienia związane z duszkami
