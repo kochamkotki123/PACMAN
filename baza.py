f217b80277fa64854e00e74da9f89b55249f3559
gaba23333 <104887032+gaba23333@users.noreply.github.com>
2022-06-02 17:16:08 +0200

    Add files via upload

---
+++ baza.py
@@ -0,0 +1,10 @@
+
+# ogólna siła sprawcza, dzięki której działa program, importujemy tu pozostałe pliki
+
+from kodzik import *
+# main odpowiada za uruchomienie kodu, F5 wciskamy w tym pliku
+
+if __name__=='__main__':
+    app = App()
+
+    app.run()
