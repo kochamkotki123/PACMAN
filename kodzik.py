bbc292931f498d028d57771ad331c94e5fa34712
gaba23333 <104887032+gaba23333@users.noreply.github.com>
2022-06-05 12:57:35 +0200

    Update kodzik.py
    Dodanie ustawień związanych z kodem

--- kodzik.py
+++ kodzik.py
@@ -1,6 +1,5 @@
 import pygame
 import sys
-from ustawienia import *
 from gracz import *
 from duszki import *
 
@@ -18,6 +17,29 @@ pygame.init()
 vec = pygame.math.Vector2
 # nazwa gry w oknie
 pygame.display.set_caption("Twój stary Pacman")
+
+from pygame.math import Vector2 as vec
+# ustawienia ekranu
+WIDTH, HEIGHT = 610, 670
+FPS = 60
+TOP_BOTTOM_BUFFER = 50
+MAZE_WIDTH, MAZE_HEIGHT = WIDTH-TOP_BOTTOM_BUFFER, HEIGHT - TOP_BOTTOM_BUFFER
+
+# ustawienia kolorów
+BLACK = (0, 0, 0)
+RED = (208, 22, 22)
+GREY = (107, 107, 107)
+WHITE = (255,255,255)
+PLAYER_COLOUR = (190,194,15)
+# ustawienia czcionki
+START_TEXT_SIZE = 16
+START_FONT = 'arial black'
+
+
+# ustawienia związane z graczem
+
+PLAYER_START_POS = 0
+# ustawienia związane z duszkami
 
 
 
