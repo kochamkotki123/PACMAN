f8ae9ebaf7f33574a91e2c23b012f011beb6a8d2
gaba23333 <104887032+gaba23333@users.noreply.github.com>
2022-06-05 13:01:21 +0200

    Update gracz.py
    dodanie ustawień

--- gracz.py
+++ gracz.py
@@ -1,5 +1,23 @@
 import pygame
-from ustawienia import *
+
+# ustawienia ekranu
+WIDTH, HEIGHT = 610, 670
+FPS = 60
+TOP_BOTTOM_BUFFER = 50
+MAZE_WIDTH, MAZE_HEIGHT = WIDTH-TOP_BOTTOM_BUFFER, HEIGHT - TOP_BOTTOM_BUFFER
+
+# ustawienia kolorów
+BLACK = (0, 0, 0)
+RED = (208, 22, 22)
+GREY = (107, 107, 107)
+WHITE = (255,255,255)
+PLAYER_COLOUR = (190,194,15)
+# ustawienia czcionki
+START_TEXT_SIZE = 16
+START_FONT = 'arial black'
+
+
+# ustawienia związane z graczem
 vec = pygame.math.Vector2
 # avatar którego nie umiem jeszcze umieścić zamiast zółtej kropki
 image= pygame.image.load('start.png')
