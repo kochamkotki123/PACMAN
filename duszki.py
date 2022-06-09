18ecfbd79435e962faba97fa49be471469e6cbe1
gaba23333 <104887032+gaba23333@users.noreply.github.com>
2022-06-09 20:33:37 +0200

    Update duszki.py

--- duszki.py
+++ duszki.py
@@ -2,6 +2,26 @@ import pygame
 import random
 from pygame.math import Vector2
 
+# ustawienia ekranu
+WIDTH, HEIGHT = 610, 670
+FPS = 60
+TOP_BOTTOM_BUFFER = 50
+MAZE_WIDTH, MAZE_HEIGHT = WIDTH-TOP_BOTTOM_BUFFER, HEIGHT - TOP_BOTTOM_BUFFER
+
+# ustawienia kolorów
+BLACK = (0, 0, 0)
+RED = (208, 22, 22)
+GREY = (107, 107, 107)
+WHITE = (255,255,255)
+PLAYER_COLOUR = (190,194,15)
+# ustawienia czcionki
+START_TEXT_SIZE = 16
+START_FONT = 'arial black'
+
+# ustawienia związane z graczem
+
+PLAYER_START_POS = Vector2(1,1)
+
 # cała klasa duszków, ich ilość, pozycje
 class Duszek:
     def __init__(self, gra, pos, number):
