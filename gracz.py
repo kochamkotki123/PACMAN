e7b32e07da28ce391558607966f4cba2646a3003
sebabezchleba <104467887+sebabezchleba@users.noreply.github.com>
2022-06-12 15:46:26 +0200

    Update gracz.py

--- gracz.py
+++ gracz.py
@@ -2,20 +2,20 @@ import pygame
 from pygame.math import Vector2
 
 # ustawienia ekranu
-WIDTH, HEIGHT = 610, 670
+SZEROKOSC, WYSOKOSC = 610, 670
 FPS = 60
-TOP_BOTTOM_BUFFER = 50
-MAZE_WIDTH, MAZE_HEIGHT = WIDTH-TOP_BOTTOM_BUFFER, HEIGHT - TOP_BOTTOM_BUFFER
+BUFOR = 50
+SZEROKOSC_LABIRYNTU, WYSOKOSC_LABIRYNTU = SZEROKOSC-BUFOR, WYSOKOSC - BUFOR
 
 # ustawienia kolorów
-BLACK = (0, 0, 0)
-RED = (208, 22, 22)
-GREY = (107, 107, 107)
-WHITE = (255,255,255)
-PLAYER_COLOUR = (190,194,15)
+CZERN = (0, 0, 0)
+CZERWIEN = (208, 22, 22)
+SZAROSC = (107, 107, 107)
+BIEL = (255,255,255)
+KOLOR_GRACZA = (190,194,15)
 # ustawienia czcionki
 START_TEXT_SIZE = 16
-START_FONT = 'arial black'
+FONT = 'arial black'
 # ustawienia związane z graczem
 PLAYER_START_POS = Vector2(1,1)
 # avatar którego nie umiem jeszcze umieścić zamiast zółtej kropki
@@ -23,7 +23,7 @@ image= pygame.image.load('start.png')
 
 class Gracz:
     def __init__(self, gra, pos):
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.screen = pygame.display.set_mode((SZEROKOSC, WYSOKOSC))
         self.gra = gra
         self.starting_pos = [pos[0], pos[1]]
         self.grid_pos = Vector2(pos[0],pos[1])
@@ -45,36 +45,28 @@ class Gracz:
         if self.dotknac_punkt():
             self.zjesc_punkt()
 
-
-# pozycjonowanie troche skomplikowane średnio ogarniam więc jeżeli macie pomysły jak to ułatwić to będe wdzięczna
-
-        self.grid_pos[0] = (self.pix_pos[0]-TOP_BOTTOM_BUFFER+self.gra.cell_width//2)//self.gra.cell_width+1
-        self.grid_pos[1] = (self.pix_pos[1]-TOP_BOTTOM_BUFFER+self.gra.cell_height//2)//self.gra.cell_height+1
-
-
+# pozycjonowanie na siatce
+        self.grid_pos[0] = (self.pix_pos[0]-BUFOR+self.gra.szerokosc_komorki//2)//self.gra.szerokosc_komorki+1
+        self.grid_pos[1] = (self.pix_pos[1]-BUFOR+self.gra.wysokosc_komorki//2)//self.gra.wysokosc_komorki+1
 
     def draw(self):
-# żółte kółko ;-;
-#już nie! :D
         self.gra.screen.blit(self.gra.pacman,(int(self.pix_pos.x)-8,  int(self.pix_pos.y)-8))
 
 
-# pozycjonowanie znowu
-
+# pozycjonowanie w pikselach
     def get_pix_pos(self):
-        return Vector2((self.grid_pos.x*self.gra.cell_width)+TOP_BOTTOM_BUFFER//2+self.gra.cell_width//2, (self.grid_pos.y*self.gra.cell_height)+ TOP_BOTTOM_BUFFER//2+self.gra.cell_height//2)
+        return Vector2((self.grid_pos.x*self.gra.szerokosc_komorki)+BUFOR//2+self.gra.szerokosc_komorki//2, (self.grid_pos.y*self.gra.wysokosc_komorki)+ BUFOR//2+self.gra.wysokosc_komorki//2)
         print(self.grid_pos, self.pix_pos)
 
-#ruszanie się
-
+#ruszanie się pacmana
     def ruch(self, kierunek):
         self.biezacy_kierunek = kierunek
 
     def czas_na_ruch(self):
-        if int(self.pix_pos.x+TOP_BOTTOM_BUFFER//2) % self.gra.cell_width == 0:
+        if int(self.pix_pos.x+BUFOR//2) % self.gra.szerokosc_komorki == 0:
             if self.kierunek == Vector2(1,0) or self.kierunek == Vector2(-1,0) or self.kierunek == Vector2(0, 0):
                 return True
-        if int(self.pix_pos.y+TOP_BOTTOM_BUFFER//2) % self.gra.cell_height == 0:
+        if int(self.pix_pos.y+BUFOR//2) % self.gra.wysokosc_komorki == 0:
             if self.kierunek == Vector2(0,1) or self.kierunek == Vector2(0,-1) or self.kierunek == Vector2(0, 0):
                 return True
         return False
